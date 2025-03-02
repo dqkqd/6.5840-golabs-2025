bf5bd12ab310b9386d1ba9a5edf0955577c7655d
Khanh Duong <dqkqdlot@gmail.com>
2025-03-02 15:35:35 +0900

    feat: implement map tasks

--- src/mr/task.go
+++ src/mr/task.go
@@ -1,6 +1,9 @@
 package mr
 
-type TaskKind int
+type (
+	TaskKind      int
+	RpcTaskStatus int
+)
 
 const (
 	MapTask TaskKind = iota
@@ -8,7 +11,22 @@ const (
 )
 
 type Task struct {
-	Kind TaskKind
-	// input for map task
-	MapInputFile string
+	Id          int
+	Kind        TaskKind
+	NReduce     int    // number of reducer
+	MapFilename string // input for map task
+
+	// private fields, to help coordinator manage tasks, these fields are not sent through rpc
+	running bool // whether a task is running
+}
+
+const (
+	Wait RpcTaskStatus = iota
+	Ok
+	Stopped
+)
+
+type RpcTask struct {
+	Task   Task
+	Status RpcTaskStatus
 }
