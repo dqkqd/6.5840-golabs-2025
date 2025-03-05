4f6697862e286914697fa260d4ddf13c35ef303f
Khanh Duong <dqkqdlot@gmail.com>
2025-03-06 04:43:17 +0900

    feat: kvsrv1 reliable

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -18,15 +18,48 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type keyValue struct {
+	value   string
+	version rpc.Tversion
+}
+
+type keyValueStore struct {
+	data map[string]keyValue
+}
+
+func (s keyValueStore) get(key string) (keyValue, bool) {
+	v, ok := s.data[key]
+	return v, ok
+}
+
+func (s keyValueStore) put(key, value string) {
+	v, ok := s.get(key)
+	if ok {
+		s.data[key] = keyValue{
+			version: v.version + 1,
+			value:   value,
+		}
+	} else {
+		s.data[key] = keyValue{
+			version: 1,
+			value:   value,
+		}
+	}
+}
+
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	store keyValueStore
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
 	// Your code here.
+	kv.store = keyValueStore{
+		data: make(map[string]keyValue),
+	}
 	return kv
 }
 
@@ -34,6 +67,17 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	v, ok := kv.store.get(args.Key)
+	if ok {
+		reply.Version = v.version
+		reply.Value = v.value
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -42,6 +86,29 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	v, ok := kv.store.get(args.Key)
+	if ok {
+		if v.version == args.Version {
+			// match version
+			kv.store.put(args.Key, args.Value)
+			reply.Err = rpc.OK
+		} else {
+			// unmatch
+			reply.Err = rpc.ErrVersion
+		}
+	} else {
+		// no keys
+		if args.Version == 0 {
+			// create a new one
+			kv.store.put(args.Key, args.Value)
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrNoKey
+		}
+	}
 }
 
 // You can ignore Kill() for this lab
