8016a0a9812c172a9feabdf06d16d1807434a3bc
Khanh Duong <dqkqdlot@gmail.com>
2025-04-13 17:49:31 +0900

    feat: logs color

--- src/raft1/util.go
+++ src/raft1/util.go
@@ -1,12 +1,86 @@
 package raft
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"time"
+)
 
 // Debugging
-const Debug = false
+const (
+	Debug          = true
+	enableLogColor = true
+)
 
-func DPrintf(format string, a ...any) {
+var debugStart time.Time
+
+type (
+	logTopic string
+	logColor string
+)
+
+const (
+	tVote         logTopic = "VOTE"
+	tAppend       logTopic = "APND"
+	tHeartbeat    logTopic = "BEAT"
+	tStart        logTopic = "STRT"
+	tApply        logTopic = "APLY"
+	tBecomeLeader logTopic = "LEAD"
+	tElection     logTopic = "ELCT"
+)
+
+const (
+	tColorReset   logColor = "\033[0m"
+	tColorBlack   logColor = "\033[30m"
+	tColorRed     logColor = "\033[31m"
+	tColorGreen   logColor = "\033[32m"
+	tColorYellow  logColor = "\033[33m"
+	tColorBlue    logColor = "\033[34m"
+	tColorMagenta logColor = "\033[35m"
+	tColorCyan    logColor = "\033[36m"
+	tColorWhite   logColor = "\033[37m"
+)
+
+func logInit() {
+	if debugStart.IsZero() {
+		debugStart = time.Now()
+		log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+	}
+}
+
+func DPrintf(topic logTopic, format string, a ...any) {
 	if Debug {
+		at := time.Since(debugStart).Microseconds()
+		at /= 100
+		prefix := fmt.Sprintf("%08d %v ", at, topic)
+
+		format = prefix + format
+
+		color := topic.color()
+		if len(color) > 0 {
+			format = string(color) + format + string(tColorReset)
+		}
+
 		log.Printf(format, a...)
 	}
+}
+
+func (t logTopic) color() logColor {
+	if enableLogColor {
+		switch t {
+		case tVote:
+			return tColorRed
+		case tAppend:
+			return tColorBlue
+		case tHeartbeat:
+			return tColorMagenta
+		case tStart:
+			return tColorYellow
+		case tApply:
+			return tColorGreen
+		case tElection:
+			return tColorCyan
+		}
+	}
+	return ""
 }
