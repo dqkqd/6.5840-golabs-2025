7ece4e2c9e8f462d14a96723cb7a4ff8e1b59faa
Khanh Duong <dqkqdlot@gmail.com>
2025-04-19 13:20:52 +0900

    refactor: separate handle success and failed append entries

--- src/raft1/raft_replication.go
+++ src/raft1/raft_replication.go
@@ -11,47 +11,14 @@ func (rf *Raft) replicate(server int, args AppendEntriesArgs) {
 
 		// do not send append tries if we are not leader
 		rf.mu.Lock()
-		if rf.state != Leader {
-			rf.mu.Unlock()
+		state := rf.state
+		rf.mu.Unlock()
+		if state != Leader {
 			return
 		}
-		rf.mu.Unlock()
 
 		reply := AppendEntriesReply{}
 		rf.sendAppendEntries(server, &args, &reply)
-
-		// appending successfully, no need to send any append entries
-		if reply.Success {
-			rf.mu.Lock()
-			defer rf.mu.Unlock()
-
-			DPrintf(tSendAppend, "S%d(%d) -> S%d(%d), append entries success", rf.me, rf.currentTerm, server, reply.Term)
-
-			// update nextIndex and matchIndex
-			rf.matchIndex[server] = args.PrevLogIndex + len(args.Entries)
-			rf.nextIndex[server] = rf.matchIndex[server] + 1
-
-			// find majority replicated entries to commit
-			for n := len(rf.log) - 1; n > rf.commitIndex; n-- {
-				// TODO: might not need this check?
-				if rf.log[n].Term == rf.currentTerm {
-					replicatedCount := 1 // self should be count
-					for i := range rf.peers {
-						if rf.me != i && rf.matchIndex[i] >= n {
-							replicatedCount++
-						}
-					}
-					// majority of servers have been replicated
-					if replicatedCount*2 > len(rf.peers) {
-						rf.commitIndex = n
-						rf.applyCommand()
-						break
-					}
-				}
-			}
-
-			return
-		}
 
 		// Rules for Servers: lower term, change to follower
 		// term has been changed from peer,
@@ -60,36 +27,79 @@ func (rf *Raft) replicate(server int, args AppendEntriesArgs) {
 			rf.mu.Lock()
 			defer rf.mu.Unlock()
 
-			DPrintf(tSendAppend, "S%d(%d) -> S%d(%d), append failed at term %d, maybe change to follower", rf.me, rf.currentTerm, server, reply.Term, args.Term)
+			DPrintf(tSendAppend, "S%d(%d) -> S%d(%d), append failed at term %d, might change to follower", rf.me, rf.currentTerm, server, reply.Term, args.Term)
 			rf.changeTerm(reply.Term)
 			return
 		}
 
-		// rejection, change nextIndex and retry
-		rf.mu.Lock()
-
-		if reply.XTerm != -1 && reply.XIndex != -1 {
-			// conflicting term
-
-			// find the last index of XTerm (the index before the first XTerm + 1)
-			lastXTermIndex := rf.findFirstIndexWithTerm(reply.XTerm+1) - 1
-
-			if lastXTermIndex >= 0 && rf.log[lastXTermIndex].Term == reply.XTerm {
-				// leader has XTerm
-				rf.nextIndex[server] = lastXTermIndex + 1
-			} else {
-				// leader doesn't have XTerm
-				rf.nextIndex[server] = reply.XIndex
-			}
+		if reply.Success {
+			rf.handleSuccessAppendEntries(server, &args, &reply)
+			// appending successfully, no need to send any append entries
+			return
 		} else {
-			// follower's log is too short
-			rf.nextIndex[server] = reply.XLen
+			rf.handleFailedAppendEntries(server, &args, &reply)
 		}
-
-		DPrintf(tSendAppend, "S%d(%d) -> S%d(%d), append rejected, nextIndex=%d, reply=%+v", rf.me, rf.currentTerm, server, reply.Term, rf.nextIndex[server], reply)
-
-		args = rf.appendEntriesArgs(rf.nextIndex[server])
-
-		rf.mu.Unlock()
 	}
+}
+
+// handle entries that was successfully sent
+func (rf *Raft) handleSuccessAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	DPrintf(tSendAppend, "S%d(%d) -> S%d(%d), append entries success", rf.me, rf.currentTerm, server, reply.Term)
+
+	// update nextIndex and matchIndex
+	rf.matchIndex[server] = args.PrevLogIndex + len(args.Entries)
+	rf.nextIndex[server] = rf.matchIndex[server] + 1
+
+	// find majority replicated entries to commit
+	for n := len(rf.log) - 1; n > rf.commitIndex; n-- {
+		// TODO: might not need this check?
+		if rf.log[n].Term == rf.currentTerm {
+			replicatedCount := 1 // self should be count
+			for i := range rf.peers {
+				if rf.me != i && rf.matchIndex[i] >= n {
+					replicatedCount++
+				}
+			}
+			// majority of servers have been replicated
+			if replicatedCount*2 > len(rf.peers) {
+				rf.commitIndex = n
+				rf.applyCommand()
+				return
+			}
+		}
+	}
+}
+
+// handle entries that was replied with failure, leader must change `nextIndex` and retry
+func (rf *Raft) handleFailedAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	// rejection, change nextIndex and retry
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	DPrintf(tSendAppend, "S%d(%d) -> S%d(%d), append entries failed, reply=%+v", rf.me, rf.currentTerm, server, reply.Term, reply)
+
+	if reply.XTerm != -1 && reply.XIndex != -1 {
+		// conflicting term
+
+		// find the last index of XTerm (the index before the first XTerm + 1)
+		lastXTermIndex := rf.findFirstIndexWithTerm(reply.XTerm+1) - 1
+
+		if lastXTermIndex >= 0 && rf.log[lastXTermIndex].Term == reply.XTerm {
+			// leader has XTerm
+			rf.nextIndex[server] = lastXTermIndex + 1
+		} else {
+			// leader doesn't have XTerm
+			rf.nextIndex[server] = reply.XIndex
+		}
+	} else {
+		// follower's log is too short
+		rf.nextIndex[server] = reply.XLen
+	}
+
+	DPrintf(tSendAppend, "S%d(%d) -> S%d(%d), append entries failed, change nextIndex=%d", rf.me, rf.currentTerm, server, reply.Term, rf.nextIndex[server])
+
+	*args = rf.appendEntriesArgs(rf.nextIndex[server])
 }
